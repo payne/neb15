676e493bda83251a5934c9a29969929113e03d98
ericssmith <eric2.71828@gmail.com>
2015-03-17 19:14:37 -0400

    Mixin

---
+++ modules/src/main/scala/com/demo/shapes/007.sc
@@ -0,0 +1,6 @@
+import com.demo.shapes.Circle6
+
+val c = Circle6(20, 0.33, 0.25, 0.5)
+
+
+
---
+++ modules/src/main/scala/com/demo/shapes/Shape2.scala
@@ -0,0 +1,19 @@
+package com.demo.shapes
+
+trait Shape2 {
+  def area: Double
+  def perimeter: Double
+}
+
+trait Color {
+  val red: Double
+  val blue: Double
+  val green: Double
+}
+
+case class Circle6(radius: Double, red: Double, blue: Double, green: Double) extends Shape2 with Color {
+  private val myPi = 3.14
+  def area = myPi * radius * radius
+  def perimeter = myPi * radius * 2
+}
+
