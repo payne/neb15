bf44d2aaf7736adb44e6c29b3d1493d6611cc061
ericssmith <eric2.71828@gmail.com>
2015-03-17 17:01:45 -0400

    Companion object and apply.

---
+++ modules/src/main/scala/com/demo/shapes/004.sc
@@ -0,0 +1,12 @@
+//import com.demo.shapes.Circle3
+
+//val c1 = Circle3.apply(20)
+
+//c1.area
+//c1.perimeter
+
+
+//val c2 = Circle3(20)
+
+//c2.area
+//c2.perimeter
---
+++ modules/src/main/scala/com/demo/shapes/Circle3.scala
@@ -0,0 +1,14 @@
+package com.demo.shapes
+
+
+class Circle3(radius: Double) {
+  private val myPi = 3.14
+
+  def area = myPi * radius * radius
+  def perimeter = myPi * radius * 2
+
+}
+
+object Circle3 {
+  def apply(radius: Double) = new Circle3(radius)
+}
