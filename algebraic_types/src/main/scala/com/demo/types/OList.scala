2a5ceb12432079c9d1ac3d0ad865cad227ed8c89
ericssmith <eric2.71828@gmail.com>
2015-03-18 13:05:46 -0500

    Map combinator as a method

---
+++ algebraic_types/src/main/scala/com/demo/types/OList.scala
@@ -0,0 +1,12 @@
+package com.demo.types
+
+
+sealed trait OList[+T] {
+  def map[U](f: T => U): OList[U] = this match {
+    case OEmpty => OEmpty
+    case OCons(x, xs) => OCons(f(x), xs.map(f))
+  }
+}
+
+case class OCons[+T](first: T, rest: OList[T]) extends OList[T]
+case object OEmpty extends OList[Nothing]
