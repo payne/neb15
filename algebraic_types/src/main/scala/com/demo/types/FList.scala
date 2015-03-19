6398e3ec485fa19db155ad25f4c992bafe5e3789
Matt Payne <Payne@MattPayne.org>
2015-03-19 13:28:47 -0500

    linked list is a recursive sum type

--- algebraic_types/src/main/scala/com/demo/types/FList.scala
+++ algebraic_types/src/main/scala/com/demo/types/FList.scala
@@ -1,5 +1,14 @@
 package com.demo.types
 
+// This is a sum type
+// this is parametrized
+
+
+// The + here means that it's covariant
+// There is an opposite called contravariance that is hardly ever seen, and it is mind twisting.
+
+// At the very top of the hierarchy is any at the bottom is nothing
+
 sealed trait FList[+T]
 case class Cons[+T](first: T, rest: FList[T]) extends FList[T]
 case object Empty extends FList[Nothing]
@@ -12,3 +21,12 @@ object FList {
     }
   }
 }
+
+// Linked list is a recursive sum type
+
+// map is very important because it is a way to do repetition in a functional language
+// map has to be inside of a trait, class, or object
+// but in the worksheet we can use map directly
+
+// Look at 003.sc to exercise this
+
