e7d1e86ecc7c17be8fac398dd1a1644434c5a263
Matt Payne <MattPayne@Solutionary.com>
2015-03-19 13:06:40 -0500

    Seperating implementation and type specification.

--- algebraic_types/src/main/scala/com/demo/shapes/Shape.scala
+++ algebraic_types/src/main/scala/com/demo/shapes/Shape.scala
@@ -4,3 +4,7 @@ sealed trait Shape
 case class Circle(radius: Double) extends Shape
 case class Rectangle(length: Double, width: Double) extends Shape
 
+// Shape is the type constructor here.
+// The implementation is not here.
+// Implementation is located in worksheet 001.sc
+// where we use this type.
