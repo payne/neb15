a666faf487638dce84656bc37c9ebe6642dd10d3
Matt Payne <MattPayne@Solutionary.com>
2015-03-19 13:15:50 -0500

    parametrized types

--- algebraic_types/src/main/scala/com/demo/types/Pair.scala
+++ algebraic_types/src/main/scala/com/demo/types/Pair.scala
@@ -1,5 +1,8 @@
 package com.demo.types
 
+// This is a product type (not a sum type)
+// demo of parametrized types
+
 case class Pair[T, S](fst: T, snd: S)
 
 case class Triple[T, S, R](_1: T, _2: S, _3: R)
