901ec7bab2d5232ebfb7864457fb0536ef91d2b8
Matt Payne <MattPayne@Solutionary.com>
2015-03-19 11:48:35 -0500

    Sometimes traits are NOT like java interfaces

--- modules/src/main/scala/com/demo/shapes/Shape.scala
+++ modules/src/main/scala/com/demo/shapes/Shape.scala
@@ -3,12 +3,14 @@ package com.demo.shapes
 
 // smells like a java interface for the moment.
 trait Shape {
+  // note that in java 5,6,7 this would not be possible!  Cool.
+  val myPi = 3.14
   def area: Double
   def perimeter: Double
 }
 
 case class Circle5(radius: Double) extends Shape {
-  private val myPi = 3.14
+  //private val myPi = 3.14
   def area = myPi * radius * radius
   def perimeter = myPi * radius * 2
 }
