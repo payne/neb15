1c8d7ba6b86467ead4c21345d001624df3747a00
ericssmith <eric2.71828@gmail.com>
2015-03-19 04:01:46 -0500

    Hello World, Unit, Processing command line args, and opening a file.

--- sideeffect/src/main/scala/com/demo/inout/Demo.scala
+++ sideeffect/src/main/scala/com/demo/inout/Demo.scala
@@ -1,7 +1,35 @@
 package com.demo.inout
 
+import java.io.FileNotFoundException
+
+import scala.io.Source
+
+// $ sbt run --warn -f=somefile.txt
+
 object Demo {
   def main (args: Array[String]): Unit = {
     println("Hello World")
+
+    process(args.toList)
+
   }
+
+  def process(args: List[String]): Boolean = {
+    args match {
+      case Nil => true // true if everything processed successfully
+      case "-f" :: x :: rest =>
+        try {
+          val file = Source.fromFile(x).mkString
+          // Entry into program here
+          println("done")
+        } catch {
+          case ex: FileNotFoundException => println("Not found: " + x)
+        }
+        process(rest)
+      case _ => false
+    }
+  }
+
 }
+
+
